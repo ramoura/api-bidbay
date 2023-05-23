ab320c7c9678c3ab9cbf6ed9e484ca503ac11289
Renato Alessandro de Moura <renatomoura@gmail.com>
2023-05-23 10:52:40 -0300

    chore: configure to deploy in GC.

---
+++ src/mainGC.ts
@@ -0,0 +1,20 @@
+import * as express from 'express';
+import {Request, Response} from 'express';
+
+const app = express();
+
+// Defina suas rotas e lógica de roteamento com o Express
+app.get('/users', (req: Request, res: Response) => {
+    // Lógica para consultar usuários
+    // ...
+    res.status(200).send('Consulta de usuários');
+});
+
+app.post('/users', (req: Request, res: Response) => {
+    // Lógica para incluir um usuário
+    // ...
+    res.status(201).send('Usuário incluído com sucesso');
+});
+
+// Exporte uma função do Google Cloud Functions que encapsula o roteamento do Express
+export const api = app;
