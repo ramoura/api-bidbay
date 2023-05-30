e6405b73dcbc7ea3be454e63fb5e96812acbdc53
Renato Alessandro de Moura <renatomoura@gmail.com>
2023-05-30 20:07:01 -0300

    chore: configure to deploy in GC.

--- src/infra/service/TokenGenerateGoogleIdentity.ts
+++ src/infra/service/TokenGenerateGoogleIdentity.ts
@@ -1,6 +1,6 @@
 import User from "../../domain/entity/User";
 import TokenGenerate from "../../domain/service/TokenGenerate";
-import {JWT} from 'google-auth-library';
+import {sign} from "jsonwebtoken";
 
 export default class TokenGenerateGoogleIdentity implements TokenGenerate {
 
@@ -16,25 +16,23 @@ export default class TokenGenerateGoogleIdentity implements TokenGenerate {
         console.log("TokenGenerateLocal generate token for user: " + user.name + " use: " + process.env.CLIENT_EMAIL);
         console.log("TokenGenerateLocal generate token for user: " + user.name + " use: " + privateKey);
 
-        const credentials = {
-            client_email: process.env.CLIENT_EMAIL,
-            private_key: privateKey,
-        };
-
-        const jwtClient = new JWT({
-            email: credentials.client_email,
-            key: credentials.private_key,
-            scopes: ['https://www.googleapis.com/auth/service-account'],
-        });
-
-        const token = await jwtClient.authorize();
-
-        console.log(token)
-
-        if (!token.access_token) {
-            throw new Error("TokenGenerateLocal generate")
-        }
-        return token.access_token;
+        return sign({
+                aud: "https://www.googleapis.com/auth/service-account",
+                iss: "https://accounts.google.com",
+                email: process.env.CLIENT_EMAIL,
+                azp: process.env.CLIENT_EMAIL,
+                email_verified: true,
+                scopes: ['https://www.googleapis.com/auth/service-account'],
+                name: user.name,
+                //email: user.email,
+                iat: Math.floor(issueDate.getTime() / 1000)
+            },
+            privateKey,
+            {
+                expiresIn: expiresIn,
+                algorithm: 'RS256'
+            }
+        );
     }
 
     async verify(token: string): Promise<any> {
